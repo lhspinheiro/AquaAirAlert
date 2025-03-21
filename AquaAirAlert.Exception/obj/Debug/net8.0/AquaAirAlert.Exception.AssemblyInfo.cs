c29e28bfe84252c8b40ac5af5b3c03733ef2affc
lhspinheiro <luishenrique0109@hotmail.com>
2025-03-21 00:36:27 -0300

    delete entites from db

--- AquaAirAlert.Exception/obj/Debug/net8.0/AquaAirAlert.Exception.AssemblyInfo.cs
+++ AquaAirAlert.Exception/obj/Debug/net8.0/AquaAirAlert.Exception.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AquaAirAlert.Exception")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1501f25332c4087007f326089e76666e49d9d1a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22b8e8ce9e2f9eeece99d4956c9fc4c129951991")]
 [assembly: System.Reflection.AssemblyProductAttribute("AquaAirAlert.Exception")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AquaAirAlert.Exception")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
